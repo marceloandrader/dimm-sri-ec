396c3a2e82c02fb55379ac4b207a30c07e44ed5a
Marcelo Andrade R <marcelo.andrade.r@gmail.com>
2017-04-12 11:54:22 -0500

    Do not lose the contributor data

--- run.sh
+++ run.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+VERSION=1.7
+
 xhost +
+
+if [ ! -f $HOME/Documents/DtsRuc.xml ]; then
+	docker create --name dimm-sri-ec-tmp marceloandrader/dimm-sri-ec:$VERSION
+	docker cp dimm-sri-ec-tmp:/opt/dimm/src/xml/data/DtsRuc.xml ~/Documents/DtsRuc.xml
+	docker rm dimm-sri-ec-tmp
+fi
 
 docker run --rm -it \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=$DISPLAY \
     -v $HOME/dimm:/dimm \
-    marceloandrader/dimm-sri-ec:1.7
+    -v $HOME/Documents/DtsRuc.xml:/opt/dimm/src/xml/data/DtsRuc.xml \
+    --name dimm-sri-ec \
+    marceloandrader/dimm-sri-ec:$VERSION
